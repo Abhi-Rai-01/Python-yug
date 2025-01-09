b4315f050a5ad34e334c211d33555fccad2971b9
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-10 00:18:40 +0530

    more about variables

---
+++ Assigning-single-value-to-multiple-variables.py
@@ -0,0 +1,11 @@
+# assign single value to multiple variables
+
+num = 23
+age = 23
+value = 23
+print(num),print(age),print(value),
+
+print("\v")
+
+num = age = value = 55
+print(num),print(age),print(value),
---
+++ creating-multiple-variables-in-one-line.py
@@ -0,0 +1,15 @@
+# creating multiple variables in one line
+
+
+#multiple lines are used for different variables
+name = "Abhi"
+age = 20
+salary = 69999.99
+print(name)
+print(age)
+print(salary)
+print("\n\n\n")
+
+# one line used for creating multiple variables, we can even write multiple print command in one line
+name, age, salary = "Supra",19,69999.90
+print(name),print(age),print(salary),
