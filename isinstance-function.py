447584a01142ec30e297b3f60caf8dbd8435772e
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-22 22:45:14 +0530

    isinstance function intro

---
+++ isinstance-function.py
@@ -0,0 +1,5 @@
+# isinstance() function
+
+age = 23
+print(isinstance(age, int))
+print(isinstance(age, float))
---
+++ type-fucntion.py
@@ -0,0 +1,25 @@
+# type function
+
+age = 23
+print(type(23))
+print(type(age))
+
+
+age = 23.3
+print(type(age))
+
+
+age = 2+3j
+print(type(age))
+
+
+age = """Abhi"""
+print(type(age))
+
+
+age = "s"
+print(type(age))
+
+
+age = [1, 2, 3]
+print(type(age))
