6dc9268119df8aeda50ef048661c1f4b8a35ae07
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-23 23:36:16 +0530

    string data type intro

---
+++ string-datatype.py
@@ -0,0 +1,10 @@
+print("hello world")
+print('hello world')
+print('''hello world''')
+print("""hello world""")
+print("\n")
+
+print("""
+Like
+Share
+Comment""")      #This a way in which we can write multi line string.
---
+++ string-datatype01.py
@@ -0,0 +1,7 @@
+name = "Abhirai"
+print(len(name))  #len() is function used to count the lenght of the string
+print(name[3])   #we are accessing the 3rd index from the string Abhirai which is assigned to name variable.
+print(name[-3])      #we are accessing the 3rd index from the end from the string Abhirai which is assigned to name variable.
+
+age = "18"
+print(type(age))
