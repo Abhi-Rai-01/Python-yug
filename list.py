dcab5fa3940180f4718f0e256843687f40e0d053
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-24 01:21:30 +0530

    sequence type intro

---
+++ list.py
@@ -0,0 +1,8 @@
+my_list=["India",20,20.5,[1,23,3],2+3j]   #list can also be listed in a list
+print(my_list)
+print(type(my_list))
+print(my_list[2])  #we are accessing the object at the 2nd index
+
+
+my_list[2]="40.5"   # This is a way with which we are changing the value at the 2nd index, because list is mutable.
+print(my_list)
---
+++ tuple.py
@@ -0,0 +1,5 @@
+t=(10,20.4,"hello",[1,2,3])
+print(type(t))
+
+t[2]="Abhirai"   # this will give error becuase tuple is immuatable.
+print(t)
