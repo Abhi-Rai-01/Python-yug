2507749e099ef72a703354d4e8bb51e269194ded
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-29 23:51:36 +0530

    operators intro

---
+++ Operators/compound-operators.py
@@ -0,0 +1,23 @@
+# Compound operators:-
+# - combination of other operator and assignment operator.
+
+# notation:- [(operator)=]
+# ex. a+=10 is same as a = a+10
+
+a = 10
+print(a)
+
+a=a+10
+print(a)
+
+b = 20
+print(b)
+
+b += 10
+print(b)
+
+c = 12
+print(c)
+
+c*=5
+print(c)
