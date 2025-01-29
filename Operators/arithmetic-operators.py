f65fb117862d1326c2bddee87ac1a70aa56dd093
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-29 22:36:14 +0530

    operators intro

---
+++ Operators/arithmetic-operators.py
@@ -0,0 +1,13 @@
+# Arithmetic Operators:-
+# - used to perform arithmetic operations on two or more operands.
+# Below are arithmetic operators:
+
+#addition, subtraction, multiplication, division
+
+print(2 + 3)
+print(2.0 + 3)   #if any one of the two operands is float then the result will also be float
+print(2 - 3)
+print(2.1 - 3)   #if any one of the two operands is float then the result will also be float
+print(2 * 3)
+print(2 * 3.0)   #if any one of the two operands is float then the result will also be float
+print(2 / 3)    #always returns float value only
