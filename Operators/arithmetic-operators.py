6557307c02fd68bff04186ae4d68c9cd5f748cd8
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-29 22:42:01 +0530

    operators intro

--- Operators/arithmetic-operators.py
+++ Operators/arithmetic-operators.py
@@ -1,6 +1,13 @@
 # Arithmetic Operators:-
 # - used to perform arithmetic operations on two or more operands.
 # Below are arithmetic operators:
+# 1) + addition
+# 2) - subtraction
+# 3) * multiplication
+# 4) / division
+# 5) % Remainder
+# 6) // floor division
+# 7) ** exponetiation
 
 #addition, subtraction, multiplication, division
 
@@ -11,3 +18,13 @@ print(2.1 - 3)   #if any one of the two operands is float then the result will a
 print(2 * 3)
 print(2 * 3.0)   #if any one of the two operands is float then the result will also be float
 print(2 / 3)    #always returns float value only
+
+print("\t")
+print("\t")
+print("\t")
+print("\t")
+
+# using variables
+num1 = 2
+num2 = 3
+print(num1 + num2)
