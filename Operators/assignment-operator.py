1f7e1039dc95f20f60fba5d1fe973951c678afca
Abhi-Rai-01 <abhirai2309@gmail.com>
2025-01-29 23:34:35 +0530

    operators intro

---
+++ Operators/assignment-operator.py
@@ -0,0 +1,6 @@
+# Assignment Operator(=):-
+# - operator used to assign result of right expression/python object to left variable
+# Syntax:  variable = expression/python object
+# example:-
+
+num = 100
